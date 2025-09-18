ae71a2067f595b674b9b85259d8b19efc83e3eda
Volodymyr Savka <savkavoldemar1@gmail.com>
2025-09-18 23:14:12 +0300

    add task 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,22 @@
 
 const container = document.querySelector('.wall');
 
-container.style.position = 'relative';
-
 const spider = document.querySelector('.spider');
 
-const width = (container.clientWidth - spider.clientWidth) / 2;
-const height = (container.clientHeight - spider.clientHeight) / 2;
+const positionSpider = () => {
+  container.style.position = 'relative';
 
-spider.style.transform = `translateX(${width}px) translateY(${height}px)`;
+  const contSize = container.getBoundingClientRect();
+  const width = spider.offsetWidth;
+  const height = spider.offsetHeight;
+
+  spider.style.position = 'absolute';
+  spider.style.left = (contSize.width - width) / 2 + 'px';
+  spider.style.top = (contSize.height - height) / 2 + 'px';
+};
+
+if (spider.complete) {
+  positionSpider();
+} else {
+  spider.addEventListener('load', positionSpider);
+}
