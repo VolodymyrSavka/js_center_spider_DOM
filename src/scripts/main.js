2e52bc521b09bd7085854166bae2b57d63ffd31e
Volodymyr Savka <savkavoldemar1@gmail.com>
2025-09-18 22:31:45 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('.wall');
+
+container.style.position = 'relative';
+
+const spider = document.querySelector('.spider');
+
+const width = (container.clientWidth - spider.clientWidth) / 2;
+const height = (container.clientHeight - spider.clientHeight) / 2;
+
+spider.style.transform = `translateX(${width}px) translateY(${height}px)`;
